35020355cfc6199b1638280806fce66d3b96d6ec
mingwen <mingwen72@gmail.com>
2017-01-23 17:23:39 +0800

    增加user.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="renderer" content="webkit">
+    <link href="css/global.css" rel="stylesheet" type="text/css">
     <title>Hizip图书管理系统</title>
     <?php
     include_once 'config/config.php';
--- user.php
+++ user.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta name="renderer" content="webkit">
     <link href="css/global.css" rel="stylesheet" type="text/css">
-    <title>登陆</title>
+    <title>Hizip-登陆</title>
     <?php
     include_once 'config/config.php';
 
